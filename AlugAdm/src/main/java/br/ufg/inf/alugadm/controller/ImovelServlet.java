fc21bfea5b71a35c1f2592492d03f238cba63b8b
pauloXtr3m <paulo-gyn@outlook.com>
2016-07-20 15:12:14 -0300

    Correção no servlet de imoveis

--- AlugAdm/src/main/java/br/ufg/inf/alugadm/controller/ImovelServlet.java
+++ AlugAdm/src/main/java/br/ufg/inf/alugadm/controller/ImovelServlet.java
@@ -16,54 +16,89 @@ import org.apache.struts.action.ActionMapping;
 
 import br.ufg.inf.alugadm.dao.ImovelDAO;
 import br.ufg.inf.alugadm.model.Imovel;
+import java.util.ArrayList;
+import javax.servlet.http.HttpSession;
 
 public class ImovelServlet extends Action {
+    
+        private static final String ACTION_CADASTRAR_IMOVEL = "SalvarImovel";
+        private static final String ACTION_VISUALIZAR_IMOVEL = "VisualizarImovel";
 
 	@Override
 	public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
 
-		String code = verifica(request, "code");
-		String tipo = verifica(request, "tipo");
-		String data = verifica(request, "data");
-		String estado = verifica(request, "estado");
-		String status = verifica(request, "status");
-		String logradouro = verifica(request, "logradouro");
-		String complemento = verifica(request, "complemento");
-		String categoria = verifica(request, "categoria");
-		String garagem = verifica(request, "garagem");
-		String valor = verifica(request, "valor");
-		String cep = verifica(request, "cep");
+		String action = "";
 
-		Imovel imovel = new Imovel();
+                if (request.getParameter("action") != null) { // Caso a action seja diferente de null ...
+                    action = request.getParameter("action");
+                } // Altera o valor inicial de "" para a passada para o servlet
+                if(action.equalsIgnoreCase(ACTION_CADASTRAR_IMOVEL)){
+                    
+                    String code = verifica(request, "code");
+                    String tipo = verifica(request, "tipo");
+                    String data = verifica(request, "data");
+                    String estado = verifica(request, "estado");
+                    String status = verifica(request, "status");
+                    String logradouro = verifica(request, "logradouro");
+                    String complemento = verifica(request, "complemento");
+                    String categoria = verifica(request, "categoria");
+                    String garagem = verifica(request, "garagem");
+                    String valor = verifica(request, "valor");
+                    String cep = verifica(request, "cep");
 
-		int codeint = Integer.parseInt(code);
-		imovel.setCode(codeint);
-		imovel.setTipo(tipo);
-		imovel.setData(data);
-		imovel.setEstado(estado);
-		imovel.setStatus(status);
-		imovel.setLogradouro(logradouro);
-		imovel.setComplemento(complemento);
-		imovel.setCategoria(categoria);
+                    Imovel imovel = new Imovel();
 
-		boolean garagembool = convertBoolean(garagem);
-		imovel.setGaragem(garagembool);
+                    int codeint = Integer.parseInt(code);
+                    imovel.setCode(codeint);
+                    imovel.setTipo(tipo);
+                    imovel.setData(data);
+                    imovel.setEstado(estado);
+                    imovel.setStatus(status);
+                    imovel.setLogradouro(logradouro);
+                    imovel.setComplemento(complemento);
+                    imovel.setCategoria(categoria);
 
-		float valorfloat = Float.parseFloat(valor);
-		imovel.setValor(valorfloat);
-		
-		imovel.setCep(cep);
+                    boolean garagembool = convertBoolean(garagem);
+                    imovel.setGaragem(garagembool);
 
-		ImovelDAO imovelDAO = null;
-		try {
-			imovelDAO = new ImovelDAO();
-			imovelDAO.register(imovel);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-                return mapping.findForward("adicionarImovel");
+                    float valorfloat = Float.parseFloat(valor);
+                    imovel.setValor(valorfloat);
 
-	}
+                    imovel.setCep(cep);
+
+                    ImovelDAO imovelDAO = null;
+                    try {
+                            imovelDAO = new ImovelDAO();
+                            imovelDAO.register(imovel);
+                    } catch (SQLException e) {
+                            e.printStackTrace();
+                    }
+                    return mapping.findForward("CadastrarImovel");
+
+                }
+                
+                //Caso a action seja de visualizar imóvel
+               
+                if(action.equalsIgnoreCase(ACTION_VISUALIZAR_IMOVEL)){
+                    
+                     HttpSession session = request.getSession();
+                     
+                    ImovelDAO imovelDao = new ImovelDAO();
+                   //acessa uma lista de imoveis no banco de dados
+                   ArrayList<Imovel> listaImovel = imovelDao.getListaImoveis();
+                   
+                   request.setAttribute("listaImovel", listaImovel);
+
+                   return mapping.findForward("VisualizarImovel");
+                }
+                
+                
+                
+                return mapping.findForward("Error");
+          }
+    
+            
+                
 
 	private boolean convertBoolean(String var) {
 
--- AlugAdm/src/main/java/br/ufg/inf/alugadm/dao/ImovelDAO.java
+++ AlugAdm/src/main/java/br/ufg/inf/alugadm/dao/ImovelDAO.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 
 public class ImovelDAO {
 
+<<<<<<< Updated upstream
 	private Connection connection;
 
 	public ImovelDAO() throws SQLException {
@@ -75,26 +76,89 @@ public class ImovelDAO {
                         imovel.setCep(resultSet.getString("cep"));
                         
                         listaImovel.add(imovel);
+=======
+    private Connection connection;
+
+    public ImovelDAO() throws SQLException {
+        FabricaConexao conexao = new FabricaConexao();
+        this.connection = conexao.getConnection();
+    }
+
+    public void register(Imovel imovel) throws SQLException {
+
+        String sql = "INSERT INTO Imovel (codigoImovel, tipo, dataCadastro, valorAlguel, status, logradouro, complemento, cidade, estado, categoria, numQuartos, garagem, cep) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+        try {
+
+            PreparedStatement statement = getConnection().prepareStatement(sql);
+
+            statement.setInt(1, imovel.getCode());
+            statement.setString(2, imovel.getTipo());
+            statement.setString(3, imovel.getData());
+            statement.setFloat(4, imovel.getValor());
+            statement.setString(5, imovel.getStatus());
+            statement.setString(6, imovel.getLogradouro());
+            statement.setString(7, imovel.getComplemento());
+            statement.setString(8, imovel.getCidade());
+            statement.setString(9, imovel.getEstado());
+            statement.setString(10, imovel.getCategoria());
+            statement.setInt(11, imovel.getNumQuartos());
+            statement.setBoolean(12, imovel.isGaragem());
+            statement.setString(13, imovel.getCep());
+
+            statement.execute();
+            statement.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            getConnection().close();
+>>>>>>> Stashed changes
         }
-                    
-                }catch(SQLException e){
-                    
-                }finally{
-                    	getConnection().close();
 
-                }
-                
-                return listaImovel;
-                
-                
-	}
+    }
 
-	public Connection getConnection() {
-		return connection;
-	}
+    public ArrayList<Imovel> getListaImoveis() throws SQLException {
+        String sql = "SELECT codigoImovel, tipo, dataCadastro, valorAlguel, status, logradouro, complemento, cidade, estado, categoria, numQuartos, garagem, cep FROM Imovel";
+        ArrayList<Imovel> listaImovel = new ArrayList<Imovel>();
 
-	public void setConnection(Connection connection) {
-		this.connection = connection;
-	}
+        try {
+            Statement statement = getConnection().createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+            Imovel imovel = new Imovel();
+            imovel.setCode(resultSet.getInt("codigoImovel"));
+            imovel.setTipo(resultSet.getString("tipo"));
+            imovel.setData(resultSet.getString("dataCadastro"));
+            imovel.setValor(resultSet.getFloat("valorAluguel"));
+            imovel.setStatus(resultSet.getString("status"));
+
+            imovel.setLogradouro(resultSet.getString("logradouro"));
+            imovel.setComplemento(resultSet.getString("complemento"));
+            imovel.setCidade(resultSet.getString("cidade"));
+            imovel.setEstado(resultSet.getString("estado"));
+            imovel.setCategoria(resultSet.getString("categoria"));
+            imovel.setNumQuartos(resultSet.getInt("numQuartos"));
+            imovel.setGaragem(resultSet.getBoolean("garagem"));
+            imovel.setCep(resultSet.getString("cep"));
+
+            listaImovel.add(imovel);
+        } catch (SQLException e) {
+
+        } finally {
+            getConnection().close();
+
+        }
+
+        return listaImovel;
+
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void setConnection(Connection connection) {
+        this.connection = connection;
+    }
 
 }
