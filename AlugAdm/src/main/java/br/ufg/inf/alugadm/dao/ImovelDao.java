6e8b62fa137e542d093ef2aff6e6334eed5036aa
juliannyas <julianny.alves@hotmail.com>
2016-07-20 15:21:40 -0300

    inclusão da interface imovelDao

---
+++ AlugAdm/src/main/java/br/ufg/inf/alugadm/dao/ImovelDao.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufg.inf.alugadm.dao;
+
+import br.ufg.inf.alugadm.model.Imovel;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author JuliannyAS
+ */
+public interface ImovelDao {
+
+    public ArrayList<Imovel> getImoveis();
+
+    public Imovel getImovelPorCodigo(int id);
+
+    public void salvarImovel(Imovel imovel);
+
+    public void excluirImovel(int id);
+
+    public void editarImovel(Imovel imovel);
+    
+    public ArrayList<String> getListaCodigoImoveis();
+}
