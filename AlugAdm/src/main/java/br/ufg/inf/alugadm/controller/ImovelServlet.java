cdcdbf10e8bd10e785bd343e584f464bdc8092a8
RenanOfugi <renangyn2010@hotmail.com>
2016-07-20 01:13:04 -0300

    Atualizando servlet de imovel

--- AlugAdm/src/main/java/br/ufg/inf/alugadm/controller/ImovelServlet.java
+++ AlugAdm/src/main/java/br/ufg/inf/alugadm/controller/ImovelServlet.java
@@ -1,44 +1,85 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.ufg.inf.alugadm.controller;
 
-import br.ufg.inf.alugadm.model.Imovel;
-import java.util.ArrayList;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import org.apache.struts.action.Action;
-import org.apache.struts.action.ActionForm;
-import org.apache.struts.action.ActionForward;
-import org.apache.struts.action.ActionMapping;
 
+import br.ufg.inf.alugadm.model.Imovel;
 
+@WebServlet("/adicionaImovel")
+public class ImovelServlet extends HttpServlet {
 
-/**
- *
- * @author JuliannyAS
- */
-public class ImovelServlet extends Action {
+	private static final long serialVersionUID = 1L;
 
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
 
-   public ActionForward VisualizarImovel(ActionMapping mapping, ActionForm form, 
-            HttpServletRequest request, HttpServletResponse response) throws Exception{
-       
-       HttpSession session = request.getSession();
-       
-       // ImovelDao imovelDao = new ImovelDao();
-       
-       //ArrayList<Imovel> listaImoveis = imovelDao.getListaImoveis();
-       
-       ArrayList<Imovel> listaImovel = new ArrayList<Imovel>();
-       
-       request.setAttribute("listaImovel", listaImovel);
-       
-       
-       return mapping.findForward("listaImovel");
-   }
+			String code = verifica(request, "code");
+			String tipo = verifica(request, "tipo");
+			String data = verifica(request,"data");
+			String estado = verifica(request, "estado");
+			String status = verifica(request,"status");
+			String logradouro = verifica(request, "logradouro");
+			String complemento = verifica(request, "complemento");
+			String categoria = verifica(request, "categoria");
+			String garagem = verifica(request, "garagem");
+			String area = verifica(request, "areaPrivativa");
+			String valor = verifica(request, "valor");
+			
+			Imovel imovel = new Imovel();
+			
+			int codeint = Integer.parseInt(code);
+			imovel.setCode(codeint);		
+			imovel.setTipo(tipo);
+			imovel.setData(data);
+			imovel.setEstado(estado);
+			imovel.setStatus(status);
+			imovel.setLogradouro(logradouro);
+			imovel.setComplemento(complemento);
+			imovel.setCategoria(categoria);
+			
+			boolean garagembool = convertBoolean(garagem);
+			imovel.setGaragem(garagembool);
+			
+			boolean areabool = convertBoolean(area);
+			imovel.setAreaPrivativa(areabool);
+			
+			float valorfloat = Float.parseFloat(valor);
+			imovel.setValor(valorfloat);
+			
+			ImovelDAO imovelDAO = new ImovelDAO();
+			imovelDAO.register(imovel);
+			
+
+	}
+	
+	private boolean convertBoolean(String var){
+		
+		boolean convert = false;
+		
+		if(var == "sim"){
+			convert = true;
+			return convert;
+		}
+		
+		return convert;
+	}
+
+	private String verifica(HttpServletRequest req, String valor) {
+
+		String resultado = req.getParameter(valor);
+
+		if (resultado == null) {
+			System.out.println("parametro vazio");
+		} else {
+			return resultado;
+		}
+
+		return null;
+	}
 
 }
