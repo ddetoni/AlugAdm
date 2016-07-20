b592c412d68a8168f00c06021b541087e5e580b0
Paulo de Oliveira Neto <paulo-gyn@outlook.com>
2016-07-20 00:12:40 -0300

    Implementada model da classe Imovel

--- AlugAdm/src/main/java/br/ufg/inf/alugadm/model/Imovel.java
+++ AlugAdm/src/main/java/br/ufg/inf/alugadm/model/Imovel.java
@@ -11,4 +11,36 @@ package br.ufg.inf.alugadm.model;
  */
 public class Imovel {
     
+    private String tipoImovel;
+    private String statusImovel;
+    private String categoria;
+    private int codigo;
+    private String logradouro;
+    private String complemento;
+    private int qntQuartos;
+    private int garagem;
+    private String estado;
+    private String dataCadastro;
+    private float valorAluguel;
+    private String areaPrivativa;
+
+    public Imovel(String tipoImovel, String statusImovel, String categoria, int codigo, String logradouro, String complemento, int qntQuartos, int garagem, String estado, String dataCadastro, float valorAluguel, String areaPrivativa) {
+        this.tipoImovel = tipoImovel;
+        this.statusImovel = statusImovel;
+        this.categoria = categoria;
+        this.codigo = codigo;
+        this.logradouro = logradouro;
+        this.complemento = complemento;
+        this.qntQuartos = qntQuartos;
+        this.garagem = garagem;
+        this.estado = estado;
+        this.dataCadastro = dataCadastro;
+        this.valorAluguel = valorAluguel;
+        this.areaPrivativa = areaPrivativa;
+    }
+    
+    public Imovel(){}
+    
+    
 }
+
