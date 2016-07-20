dff9b507d3a5fdc4c2d9824c02b9f6953b195cde
Paulo de Oliveira Neto <paulo-gyn@outlook.com>
2016-07-20 00:13:22 -0300

    Implementado model da classe Locatario

--- AlugAdm/src/main/java/br/ufg/inf/alugadm/model/Locatario.java
+++ AlugAdm/src/main/java/br/ufg/inf/alugadm/model/Locatario.java
@@ -10,5 +10,32 @@ package br.ufg.inf.alugadm.model;
  * @author JuliannyAS
  */
 public class Locatario {
+    private String nome;
+    private String cpf; 
+    private String identidade, orgao; 
+    private String sexo;
+    private String dataNascimento;
+    private String tipoPessoa;
+    private String email;
+    private String telefone;
+    private String tipoRecebimento;
+    private String dataCadastro;
+
+    public Locatario(String nome, String cpf, String identidade, String orgao, String sexo, String dataNascimento, String tipoPessoa, String email, String telefone, String tipoRecebimento, String dataCadastro) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.identidade = identidade;
+        this.orgao = orgao;
+        this.sexo = sexo;
+        this.dataNascimento = dataNascimento;
+        this.tipoPessoa = tipoPessoa;
+        this.email = email;
+        this.telefone = telefone;
+        this.tipoRecebimento = tipoRecebimento;
+        this.dataCadastro = dataCadastro;
+    }
     
+    public Locatario(){}
+   
 }
+
